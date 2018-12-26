f9b6a630320fcc7b45ca7de217a3c3f7bd536cdc
Lance Crandall <39169398+tgakk@users.noreply.github.com>
2018-12-26 09:18:12 -0700

    Update AccountController.cs

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -14,7 +14,7 @@ namespace WishList.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        public AccountController(UserManager(ApplicationUser) userManager, SignInManager(ApplicationUser) signInManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
